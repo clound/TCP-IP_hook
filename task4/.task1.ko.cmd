a4d174c1042b9fdd18817fafce791d91179aa3dc
njchen1 <964222602@qq.com>
2014-11-03 18:39:13 +0800

    tcp/ip hook create

--- task4/.task1.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/chen/sx2/7章/sx4/task4/task1.ko := ld -r -m elf_x86_64 -T /usr/src/linux-headers-3.13.0-35-generic/scripts/module-common.lds --build-id  -o /home/chen/sx2/7章/sx4/task4/task1.ko /home/chen/sx2/7章/sx4/task4/task1.o /home/chen/sx2/7章/sx4/task4/task1.mod.o
